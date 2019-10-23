338a5b23e853fdd08b60a7d968348bcaa84a5590
hlgr360 <holger@launchd.de>
2019-10-23 19:10:09 +0200

    Only backing up once on idle

--- files/auto_shutoff.py
+++ files/auto_shutoff.py
@@ -4,23 +4,51 @@ import time
 import json
 import os.path
 import urllib2
+import subprocess
+import sys
 
 server = MinecraftServer.lookup("localhost:25565")
 status = server.status()
 
-if os.path.exists('/last_activity'):
-	f = open('/last_activity', 'r+')
+if os.path.exists('/tmp/mc_last_activity'):
+	f = open('/tmp/mc_last_activity', 'r+')
 
 	if status.players.online:
 		f.seek(0)
 		f.write(str(time.time()))
 		f.truncate()
+
+		if os.path.exists("/tmp/mc_backup"):
+  			os.remove("/tmp/mc_backup")
+
 	else:
 		old_time = float(f.read()) 
 		time_past = time.time() - old_time
 		if time_past > (30*60):
-			req = urllib2.urlopen('<SERVER DESTROY LAMBDA FUNCTION>')
+			if not os.path.exists('/tmp/mc_backup'):
+				p = open('/tmp/mc_backup', 'w')
+				p.write(str(time.time()))
+
+				check_call(['aws', 's3', 'sync', 'minecraft/', sys.argv[1], '--delete'])
+			
+			#req = urllib2.urlopen('<SERVER DESTROY LAMBDA FUNCTION>')
+
 else:
-	f = open('/last_activity', 'w')
+	f = open('/tmp/mc_last_activity', 'w')
 	f.write(str(time.time()))
 	
+def check_call(args):
+    """Wrapper for subprocess that checks if a process runs correctly,
+    and if not, prints stdout and stderr.
+    """
+    proc = subprocess.Popen(args,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        cwd='/tmp')
+    stdout, stderr = proc.communicate()
+    if proc.returncode != 0:
+        print(stdout)
+        print(stderr)
+        raise subprocess.CalledProcessError(
+            returncode=proc.returncode,
+            cmd=args)
--- files/mc-setup.sh
+++ files/mc-setup.sh
@@ -5,7 +5,7 @@ sudo yum install java-1.8.0 -y
 sudo yum remove java-1.7.0-openjdk -y
 
 mkdir minecraft
-aws s3 sync s3://$1 minecraft/ --delete
+aws s3 sync s3://$1 minecraft/
 
 # install minecraft if this is the first time
 if [ ! -f "minecraft/eula.txt" ]; then
@@ -27,6 +27,6 @@ fi
 # install minecraft status 
 sudo pip install mcstatus
 
-# insert backup and auto-shutoff into cron tab
-crontab -l | { cat; echo "*/5 * * * * aws s3 sync minecraft/ s3://$1"; } | crontab -
-crontab -l | { cat; echo "*/5 * * * * python auto_shutoff.py"; } | crontab -
+# insert auto-shutoff into cron tab
+# crontab -l | { cat; echo "*/5 * * * * aws s3 sync minecraft/ s3://$1"; } | crontab -
+crontab -l | { cat; echo "*/5 * * * * python auto_shutoff.py s3://$1"; } | crontab -
